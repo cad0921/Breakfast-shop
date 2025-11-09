b4a25cb06e2904280769ea09d039ed820bdcf4ae
cad0921 <63990831+cad0921@users.noreply.github.com>
2025-11-09 22:00:32 +0800

    Move preset table view model into compiled models file

--- Models/BackstageSQL.cs
+++ Models/BackstageSQL.cs
@@ -172,4 +172,13 @@ namespace breakfastshop.Models
             return list;
         }
     }
+
+    public class PresetTableViewModel
+    {
+        public string Id { get; set; }
+        public string ShopId { get; set; }
+        public int? Number { get; set; }
+        public string Zone { get; set; }
+        public string ShopName { get; set; }
+    }
 }
